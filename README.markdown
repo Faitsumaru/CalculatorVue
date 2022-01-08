6d4c7ca47845d316ff3692c1a977b475f09f139c
Gleb <63464635+Faitsumaru@users.noreply.github.com>
2022-01-08 17:26:21 +0300

    calc_vueCMMT

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calculator Vue.js !
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Faitsu/pen/ZEOWpzw](https://codepen.io/Faitsu/pen/ZEOWpzw).
+
+
---
+++ src/script.js
@@ -0,0 +1,29 @@
+var vue = new Vue({
+  el: '#app',
+  data: {
+    result: '',
+    numbers: [1,2,3,4,5,6,7,8,9,0],
+    operations: ['+','-','*','/'],
+  },
+  methods: {
+    input: function(char) {
+      this.result = this.result.toString();
+      this.result+=char;
+    },
+    reset: function() {
+      this.result = '';
+    },
+    percent: function() {
+      this.result /= 100;
+    },
+    pow: function() {
+      this.result*=this.result;
+    },
+    sqrt: function() {
+      this.result = Math.sqrt(this.result);
+    },
+    calc: function() {
+      this.result = eval(this.result);
+    }
+  }
+})
